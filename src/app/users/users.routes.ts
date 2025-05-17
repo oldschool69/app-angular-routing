07c57839b16df338fb68a2c0c9050dce616b66f6
Oldschool69 <flavio.marcondes@gmail.com>
2025-05-17 12:35:56 -0300

    lazy loading a service in child routes

--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -1,27 +1,33 @@
 import { Routes } from '@angular/router';
 
-
 import {
   canLeaveEditPage,
   NewTaskComponent,
 } from '../tasks/new-task/new-task.component';
 import { TasksComponent } from '../tasks/tasks.component';
+import { TasksService } from '../tasks/tasks.service';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'tasks',
-    pathMatch: 'prefix',
-  },
-  {
-    path: 'tasks',
-    component: TasksComponent,
-    // loadComponent: () =>
-    //   import('../tasks/tasks.component').then((mod) => mod.TasksComponent), //Using lazy loading to load the component (default is eager)
-  },
-  {
-    path: 'tasks/new',
-    component: NewTaskComponent,
-    canDeactivate: [canLeaveEditPage],
+    providers: [TasksService], //lazy loading service to all routes that need it
+    children: [
+      {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'prefix',
+      },
+      {
+        path: 'tasks',
+        component: TasksComponent,
+        // loadComponent: () =>
+        //   import('../tasks/tasks.component').then((mod) => mod.TasksComponent), //Using lazy loading to load the component (default is eager)
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+        canDeactivate: [canLeaveEditPage],
+      },
+    ],
   },
 ];
